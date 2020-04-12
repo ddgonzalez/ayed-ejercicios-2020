d9569ce5774a234762114297c854948150e2a80e
ddgonzalez <49598132+ddgonzalez@users.noreply.github.com>
2020-04-12 01:55:56 -0300

    Update ej_9.cpp

--- ej_9.cpp
+++ ej_9.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 int main() {
    int aaaa,mm,dd;
+	int fechaunificada;
 
 
 	cout << "Ingrese un valor de 4 digitos" << endl;
@@ -19,9 +20,9 @@ int main() {
    cin>>mm;
    cout << "Ingrese un valor de 2 digitos" << endl;
    cin>>dd;
+   fechaUnificada = dia + mes * 100 + agno * 10000;
 
-
-   cout<<"la fecha unificada es"<<aaaa<<mm<<dd<<endl;
+   cout<<"la fecha unificada es"<<afechaunificada;
 
 
 
