0f05e811d99695b61db4a2c9425d3a4bcf29b717
ddgonzalez <49598132+ddgonzalez@users.noreply.github.com>
2020-04-12 05:32:41 -0300

    Add files via upload

--- ej_12.cpp
+++ ej_12.cpp
@@ -7,9 +7,31 @@
 //============================================================================
 
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
-
+//Dados dos enteros, informar su producto calculándolo mediantes sumas sucesivas.
+//Considerar que los valores ingresados serán números positivos o cero.
+//Considerar que los valores ingresados también podrían ser negativos
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+       int a, b, suma = 0;
+       int contador = 1;
+
+       //Pide y lee los numeros
+       cout << "Digite un numero entero: "; cin >> a;
+       cout << "Digite un numero entero: "; cin >> b;
+
+       //Ciclo while que realizara una multiplicacion mediante sumas sucesivas. Ej: 5*2, lo que hace es sumar 2 veces 5.
+       while ( contador <= b ) //Mientras que contador sea menor o igual que b
+       {
+           suma += a; //suma = suma + numero
+           contador++; //Postincrementa contador
+       } //Fin de while
+
+       cout << "\nEl producto calculado mediante sumas sucesivas es: " << suma << endl; //Imprime el resultado
+
+
+
+
 	return 0;
 }
